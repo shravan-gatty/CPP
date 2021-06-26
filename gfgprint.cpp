fecb686e615c1d04caccd9ce0b5b46cf908bbc9f
shravan-gatty <shravangatty2@gmail.com>
2021-06-26 13:42:52 +0530

    first commit

---
+++ gfgprint.cpp
@@ -0,0 +1,45 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "hello world" << endl<< "GFG";
+//     return 0;
+// }
+
+//2
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x=10,y=20;
+//     cout<<x<<" "<<y;
+//     return 0;
+// }
+
+//3 to print the sqaure of number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"enter the x  ";
+//     cin>>x;
+//     cout<<"sqaure of x is 25"<<(x*x);
+//     return 0;
+// }
+
+//4  multiplication of x and y
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter x "<<endl;
+    cin>>x;
+    cout<<"enter y "<<endl;
+    cin>>y;
+    cout<<"multiplication of x and y is "<<(x*y)<<endl;
+    return 0;
+
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# write program everyday
+
+It is a simple repositry to write a program everday it become a habit if you 
+write everday 
